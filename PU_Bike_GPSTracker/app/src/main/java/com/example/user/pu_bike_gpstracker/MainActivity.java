29719b0027803b5db8ecccb610249ce014e419c8
GuanYu914 <aa774281@gmail.com>
2017-06-29 00:27:24 +0800

    ACCESS MySQL specific DB GPS Info, then display on googleMap.

--- PU_Bike_GPSTracker/app/src/main/java/com/example/user/pu_bike_gpstracker/MainActivity.java
+++ PU_Bike_GPSTracker/app/src/main/java/com/example/user/pu_bike_gpstracker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.user.pu_bike_gpstracker;
 
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,37 +14,135 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.example.user.pu_bike_gpstracker.R.id.map;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback
+{
+
+    private GoogleMap                  PU_Bike_Map;
+    private Handler                    HttpReq_Timer;
+    private String                     TAG = MainActivity.class.getSimpleName();
+    private static String              url = "http://140.128.13.39/App/db_view.php";
+    private static String              No_CAR;
+    private static double              lat;
+    private static double              lon;
+
+    private final int                  interval = 1000; //1000ms = 1s
+
+    ArrayList<HashMap<String, String>> GPS_Info_List;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        GPS_Info_List = new ArrayList<>();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap googleMap)
+    {
+        PU_Bike_Map = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        HttpReq_Timer = new Handler();
+        final Runnable runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Log.d(TAG, "run was called.");
+                new HttpReq().execute();
+                LatLng pos = new LatLng(lat, lon);
+                PU_Bike_Map.addMarker(new MarkerOptions().position(pos).title(No_CAR+"'s position."));
+                PU_Bike_Map.moveCamera(CameraUpdateFactory.newLatLng(pos));
+                HttpReq_Timer.postDelayed(this, interval);
+            }
+        };
+
+        HttpReq_Timer.postDelayed(runnable, interval);
+    }
+
+
+    class HttpReq extends AsyncTask<Void, Void, Void>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+        }
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            HttpHandler sh = new HttpHandler();
+
+            String jsonStr = sh.makeServiceCall(url);
+
+            if(jsonStr != null)
+            {
+                try
+                {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+                    JSONArray GPS_Info_Arr = jsonObject.getJSONArray("GPS_Info");
+
+                    for(int i=0; i< GPS_Info_Arr.length(); i++)
+                    {
+                        JSONObject object = GPS_Info_Arr.getJSONObject(i);
+
+                        String No_CAR = object.getString("No_CAR");
+                        String Latitude = object.getString("Latitude");
+                        String Longitude = object.getString("Longitude");
+
+                        HashMap<String, String> GPS_Info = new HashMap<>();
+
+                        GPS_Info.put("No_CAR", No_CAR);
+                        GPS_Info.put("Latitude", Latitude);
+                        GPS_Info.put("Longitude", Longitude);
+
+                        GPS_Info_List.add(0 ,GPS_Info);
+                    }
+                }
+                catch (final JSONException e)
+                {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            Toast.makeText(getApplicationContext(), "Json parsing error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else
+            {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Couldn't get json from server. Check LogCat for possible",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            No_CAR = GPS_Info_List.get(0).get("No_CAR");
+            lat = Double.parseDouble(GPS_Info_List.get(0).get("Latitude"));
+            lon = Double.parseDouble(GPS_Info_List.get(0).get("Longitude"));
+        }
     }
 }
