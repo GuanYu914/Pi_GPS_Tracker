db49a9a6832c229ca9d773a5d19c4f8c4da1738a
GuanYu914 <aa774281@gmail.com>
2017-06-30 14:16:58 +0800

    Program cause Segmentation Fault because include external library.

--- workspace/src/GPS_tracker.c
+++ workspace/src/GPS_tracker.c
@@ -1,14 +1,18 @@
+#include <mcheck.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <gps.h>
 #include <mysql/mysql.h>
 //Macros
-#define HOST_NAME       "140.128.13.39"
-#define DB_USER         "Bike_manager"
-#define DB_USER_PWSD    "manager"
-#define DB_NAME         "PU_Bike"
-#define HOST_PORT       7777
+#define HOST_NAME             "140.128.13.39"
+#define DB_USER               "Bike_manager"
+#define DB_USER_PWSD          "manager"
+#define DB_NAME               "PU_Bike"
+#define HOST_PORT             7777
 //Instruction Macros
 #define TABLE_NAME            "GPS_Info"
 #define COL_NAME_1            "Latitude"
@@ -16,62 +20,65 @@
 #define SEARCH_COL_NAME       "No_CAR"
 #define SEARCH_COL_NAME_VALUE "\"PU_001\""
 //Functions
-char   *DoubleToString(double);
-char   *Substring(char *, int, int);
-double *GetLocation(void);
-void    mysql_Connect(void);
-void    mysql_InstructionQuery(char *);
-char   *mysql_InstructionMerge_1(char *, char *, char *, char *, char *, char *, char *);
+void   DoubleToString(double, char *);
+void   gps_Location(void);
+void   mysql_Connect(void);
+void   mysql_InstructionQuery(char *);
+void   mysql_InstructionMerge_1(char *, char *, char *, char *, char *, char *, char *);
+void   SIGINT_Handler(int);
 //Global Objects
-MYSQL mysql;
-loc_t gps_data;
+MYSQL  mysql;
+//Global variable
+loc_t  gps_data;
+static volatile int keepRunning = 1;
+char   lat[10];
+char   lon[10];
+char   newInstruction[50];
+double loc_arr[2];
+
 
 int main(void)
 {
+	//trace memory usage
+	mtrace();
+	//initialize gps
 	gps_init();
+	//initialize signal handler
+	signal(SIGINT, SIGINT_Handler);
+	//connect to specific mysql
 	mysql_Connect();
-	while(1)
+	//get real location and transfer info to mysql table
+	while(keepRunning)
 	{
-		double lat = GetLocation()[0];
-		double lon = GetLocation()[1];
-		mysql_InstructionQuery(mysql_InstructionMerge_1(TABLE_NAME, COL_NAME_1, DoubleToString(lat), COL_NAME_2, DoubleToString(lon), SEARCH_COL_NAME, SEARCH_COL_NAME_VALUE));
+		gps_Location();
+		DoubleToString(loc_arr[0], lat);
+		DoubleToString(loc_arr[1], lon);
+		mysql_InstructionMerge_1(TABLE_NAME, COL_NAME_1, lat, COL_NAME_2, lon, SEARCH_COL_NAME, SEARCH_COL_NAME_VALUE);
+		mysql_InstructionQuery(newInstruction);
 	}
-	mysql_close(&mysql);	
+
 	return EXIT_SUCCESS;
 }
 
-char *DoubleToString(double x)
+void SIGINT_Handler(int signum)
 {
-	char *str = malloc(20 * sizeof(char));
-	sprintf(str, "%lf", x);
-	return str;
+	keepRunning = 0;
+	printf("Process %d got signal %d\n", getpid(), signum);
+	printf("Kill process. \n");
+	muntrace();
+	kill(getpid(), signum);
+	exit(EXIT_FAILURE);
 }
 
-char *Substring(char *string, int pos, int len)
+void DoubleToString(double data, char *output)
 {
-	char *ptr;
-	int c;
-	ptr = malloc(len+1);
-	if(ptr == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-	for(c = 0; c < len; c++ )
-	{
-		*(ptr + c) = *((string+pos-1)+c);	
-	}
-	*(ptr+c) = '\0';
-	
-	return ptr;
+	sprintf(output, "%lf", data);
 }
-
-double *GetLocation(void)
+void gps_Location(void)
 {
-	double *loc_arr = malloc(2 * sizeof(double));
 	gps_location(&gps_data);
 	loc_arr[0] = gps_data.latitude;
 	loc_arr[1] = gps_data.longitude;
-	return loc_arr;
 }
 
 void mysql_Connect(void)
@@ -100,26 +107,22 @@ void mysql_InstructionQuery(char *instruction)
 	}		
 }
 
-char *mysql_InstructionMerge_1(char *table_name, char *col_name_1, char *col_value_1, char *col_name_2, char *col_value_2, char *search_col, char *search_col_name)
+void mysql_InstructionMerge_1(char *table_name, char *col_name_1, char *col_value_1, char *col_name_2, char *col_value_2, char *search_col, char *search_col_name)
 {
 	/*This function only support to "UPDATE TABLE_NAME COL_NAME = , COL_NAME = WHERE COL_NAME = " instruction.*/
-	
-	char *newInstruction = malloc(45 * sizeof(char));
 
-	strcpy(newInstruction, "UPDATE ");
-	strcat(newInstruction, table_name);
-	strcat(newInstruction, " SET ");
-	strcat(newInstruction, col_name_1);
-	strcat(newInstruction, " = ");
-	strcat(newInstruction, col_value_1);
-	strcat(newInstruction, ", ");
-	strcat(newInstruction, col_name_2);
-	strcat(newInstruction, " = ");
-	strcat(newInstruction, col_value_2);
-	strcat(newInstruction, " WHERE ");
-	strcat(newInstruction, search_col);
-	strcat(newInstruction, " = ");
-	strcat(newInstruction, search_col_name);	
-	
-	return newInstruction;
+	strncpy(newInstruction, "UPDATE ", 8);
+	strncat(newInstruction, table_name, strlen(table_name) + 1);
+	strncat(newInstruction, " SET ", 5);
+	strncat(newInstruction, col_name_1, strlen(col_name_1) + 1);
+	strncat(newInstruction, " = ", 4);
+	strncat(newInstruction, col_value_1, strlen(col_value_1) + 1);
+	strncat(newInstruction, ", ", 4);
+	strncat(newInstruction, col_name_2, strlen(col_name_2) + 1);
+	strncat(newInstruction, " = ", 4);
+	strncat(newInstruction, col_value_2, strlen(col_value_2) + 1);
+	strncat(newInstruction, " WHERE ", 8);
+	strncat(newInstruction, search_col, strlen(search_col) + 1);
+	strncat(newInstruction, " = ", 4);
+	strncat(newInstruction, search_col_name, strlen(search_col_name) + 1);	
 }
